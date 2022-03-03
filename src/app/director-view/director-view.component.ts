2d4ac4912d1a5e93cd4a281db12e29159589575f
emmanuel818 <emmanuelg8182001@gmail.com>
2022-03-03 13:13:19 -0600

    Updated component

--- src/app/director-view/director-view.component.ts
+++ src/app/director-view/director-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-view',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+      Birthyear: string;
+      Deathyear: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
