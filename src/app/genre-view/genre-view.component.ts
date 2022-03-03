e0c7adbb3bdb8a1b71f7baf8af55adf4aec97e07
emmanuel818 <emmanuelg8182001@gmail.com>
2022-03-03 13:14:00 -0600

    update

--- src/app/genre-view/genre-view.component.ts
+++ src/app/genre-view/genre-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-view',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreViewComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Description: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
+
